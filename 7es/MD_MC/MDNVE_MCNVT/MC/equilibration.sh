5c53f8093558f3d21dd48c5ffe097c7574448eb4
Riccardo-Martelli <riccardomartelli97@gmail.com>
2023-10-09 19:54:30 +0200

    Second commit

--- 7es/MD_MC/MDNVE_MCNVT/MC/equilibration.sh
+++ 7es/MD_MC/MDNVE_MCNVT/MC/equilibration.sh
@@ -1,5 +1,5 @@
-rm equlibration_temp.dat equlibration_pot.dat output_etot.dat output_press.dat output_temp.dat output_ekin.dat output_epot.dat output_gdr_MC.dat output_gdr_MD.dat output_gdr_final.dat
+rm equlibration_temp.dat equlibration_pot.dat output_etot.dat output_press.dat output_temp.dat output_ekin.dat output_epot.dat output_gdr_MD.dat output_gdr_MD.dat output_gdr_final.dat
 
-touch equlibration_temp.dat equlibration_pot.dat output_etot.dat output_press.dat output_temp.dat output_ekin.dat output_epot.dat output_gdr_MC.dat output_gdr_MD.dat 
+touch equlibration_temp.dat equlibration_pot.dat output_etot.dat output_press.dat output_temp.dat output_ekin.dat output_epot.dat output_gdr_MD.dat output_gdr_MD.dat 
 
 ./NVE_NVT.exe input.gas && ./NVE_NVT.exe input.liquid && ./NVE_NVT.exe input.solid
